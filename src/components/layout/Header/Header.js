dd9015b4d88864527d098e39f2d0968decaff5ed
unknown <kowalczyk.przemek@wp.pl>
2021-05-14 23:54:36 +0200

    Fix Footer & Add Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,37 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {NavLink, Link} from 'react-router-dom';
 
-import clsx from 'clsx';
-
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+//import { connect } from 'react-redux';
+//import { getUserInfo, setUserOnline, setUserOffline } from '../../../redux/userRedux.js';
 
 import styles from './Header.module.scss';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Icon from '../../common/Icon/Icon';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Header</h2>
-    {children}
-  </div>
-);
+const Component = ({user, setOnline, setOffline}) => {
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
+  return (
+    <div className={styles.root}>
+      <Grid>
+        <Row between="md" middle="xs">
+          <Col md={3} lg={2}>
+            <Link to='/'>
+              <div className={styles.logo}>
+                <Icon name='paper-plane' />
+                <span className={styles.name}>handicraft shop</span>
+              </div>
+            </Link>
+          </Col>
+          <Col md={6}>
+            <nav>
+              {/* user.status ? */ <NavLink to='/post/add' activeClassName='active'>Add Post</NavLink> /* : null */ }
+              {/* user.status ? */ <NavLink to='/post' activeClassName='active'>My Post&apos;s</NavLink> /* : null */ }
+              {/* user.status ? */ <NavLink to='/post' activeClassName='active' onClick={() => setOffline(false)}>Logout</NavLink> /* : null */ }
+              {/* !user.status ? */ <NavLink to='/auth/google' activeClassName='active' onClick={() => setOnline(true)}>Login with Google</NavLink> /* : null */ }
+            </nav>
+          </Col>
+        </Row>
+      </Grid>{/*  */}
+    </div>
+  );  
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+Component.propTypes = {
+  user: PropTypes.object,
+  setOnline: PropTypes.func,
+  setOffline: PropTypes.func,
+};
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+/* const mapStateToProps = state => ({
+  user: getUserInfo(state),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const mapDispatchToProps = dispatch => ({
+  setOnline: () => dispatch(setUserOnline()),
+  setOffline: () => dispatch(setUserOffline()),
+});
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component); */
 
 export {
   Component as Header,
-  // Container as Header,
+  //Container as Header,
   Component as HeaderComponent,
 };
